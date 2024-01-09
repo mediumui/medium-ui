ba0ac3d4b533104a58199d6a754835e68aa00d9a
mediumui <mediumui@163.com>
2024-01-09 15:09:12 +0800

    fix:组件库网页配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,19 @@
 module.exports = {
   base: "/medium-ui/",
   title: "medium-ui组件库",
-  description: "Just playing around",
+  description: "medium-ui使用文档",
+  themeConfig: {
+    searchMaxSuggestions: 10,
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "github", link: "https://github.com/mediumui/medium-ui" },
+      { text: "API", link: "https://github.com/mediumui/medium-ui" },
+    ],
+    sidebar: [
+      {
+        title: "安装",
+        path: "/", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+      },
+    ],
+  },
 }
