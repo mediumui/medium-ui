235b945431aff116a5375919b96295570264aeca
mediumui <mediumui@163.com>
2024-01-11 20:00:52 +0800

    Update Md5.vue

--- doc/docs/.vuepress/components/String/Md5.vue
+++ doc/docs/.vuepress/components/String/Md5.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="md5">
-    <div>原始文本：{{ message }}</div>
-    <el-button @click="toMd5">转换md5</el-button>
+    <div class="md5-content">
+      <el-input
+        placeholder="请输入文本"
+        v-model="message"
+      >
+      </el-input>
+      <el-button
+        @click="toMd5"
+        type="primary"
+      >
+        转换md5
+      </el-button>
+    </div>
     <div>转换结果： {{ md5Str }}</div>
   </div>
 </template>
@@ -27,7 +38,16 @@ export default {
   display: flex;
   flex-direction: column;
 }
+.md5-content {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 12px;
+}
 .el-button {
   width: fit-content;
+  margin-left: 10px;
+}
+.el-input {
+  width: 250px;
 }
 </style>
